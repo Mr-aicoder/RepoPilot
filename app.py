773141968685a0ee30b4ccfa969e717e2e608f06
Sachin A. Tupsamundar <165402626+Mr-aicoder@users.noreply.github.com>
2025-07-28 18:10:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+# main.py (or your Streamlit app's entry file)
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
+
+
 import streamlit as st
 import os
 import requests
