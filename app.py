e33c54a64b816576a9f53221031476f772f38538
Sachin Tupsamundar <165402626+Mr-aicoder@users.noreply.github.com>
2026-01-19 06:34:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-import streamlit as st
+import streamlit as st 
 import os
 import requests
 from langchain_core.prompts import ChatPromptTemplate
